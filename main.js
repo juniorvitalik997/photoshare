423adc74809d9736aba8ace2be74800585ad52ce
Shutiak Vitalii <122994426+juniorvitalik997@users.noreply.github.com>
2023-10-29 19:04:27 +0200

    new

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
+let data = [
+  {
+    name: 'fotosesia1',
+    category:'portret',
+    folder: 'fotosesia1',
+    count: 5
+  },
+  {
+    name: 'fotosesia2',
+    category:'portret',
+    folder: 'fotosesia2',
+    count: 5
+  },
+  {
+    name: 'fotosesia3',
+    category:'portret',
+    folder: 'fotosesia3',
+    count: 5
+  },
+  {
+    name: 'fotosesia4',
+    category:'wedding',
+    folder: 'fotosesia4',
+    count: 5
+  },
+  {
+    name: 'fotosesia5',
+    category:'wedding',
+    folder: 'fotosesia5',
+    count: 5
+  },
+  {
+    name:'fotoses6',
+    category:'portret',
+    folder: 'fotoses6',
+    count: 3
+  }
+];
+
+
 $(document).ready(function(){
   setTimeout(function(){
     $('.preloader').css('top','-110vh')
@@ -61,20 +101,20 @@ let currentPhoto = 1;
 $('#nextPhoto').click(function(){
     if(currentPhoto < photoCount){
         currentPhoto++
-    $('.sliderContainer').css('backgroundImage',`url(./img/${currentPhoto}.jpeg)`)
+    $('.sliderContainer').css('backgroundImage',`url(./img/${currentPhoto}.JPG)`)
     }else{
         currentPhoto = 1
-        $('.sliderContainer').css('backgroundImage',`url(./img/${currentPhoto}.jpeg)`)
+        $('.sliderContainer').css('backgroundImage',`url(./img/${currentPhoto}.JPG)`)
     }
     $('.currentSlide').text(currentPhoto)
 })
 $('#prewPhoto').click(function(){
     if(currentPhoto > 1){
         currentPhoto--
-    $('.sliderContainer').css('backgroundImage',`url(./img/${currentPhoto}.jpeg)`)
+    $('.sliderContainer').css('backgroundImage',`url(./img/${currentPhoto}.JPG)`)
     }else{
         currentPhoto = 3
-        $('.sliderContainer').css('backgroundImage',`url(./img/${currentPhoto}.jpeg)`)
+        $('.sliderContainer').css('backgroundImage',`url(./img/${currentPhoto}.JPG)`)
     }
     $('.currentSlide').text(currentPhoto)
 })
@@ -95,4 +135,50 @@ $('#exit').click(function(){
   $('#exit').css('display','none')
   $('.sliderContainer').css('display','flex');
   $('footer').css('display','flex');
+})
+// $('#portret').click(function(){
+//   $('.portretContainer').empty();
+//   $('.sliderContainer').css('display','none')
+//   for(let el of data){
+//     if (el.category == 'portret'){
+//       $('.portretContainer').append(`<div class = 'photosesionItem' style="background-image: url(./img/${el.folder}/1.JPG)";></div>`)
+//     }
+//   }
+//   console.log('ok')
+  
+
+// })
+let categoryExplaine = {
+  portret: 'Портрети',
+  wedding: 'Весілля'
+
+};
+$('.navItem').click(function(e){
+  let category = e.target.id;
+  $('.portretContainer').empty();
+console.log(category)
+  $('.sliderContainer').css('display','none')
+  $('.portretContainer').css('display','flex')
+  $('.portretHeaderContainer').text(`${categoryExplaine[`${category}`]}`)
+  for(let el of data){
+    if (el.category == category){
+      $('.portretContainer').append(`<div class = 'photosesionItem' style="background-image: url(./img/${el.folder}/1.JPG)";></div>`)
+    }
+  }
+  $('.photosesionItem').click(function(e){
+    
+    let bgImg = e.target.style.backgroundImage;
+    let startSlice = bgImg.indexOf('img/')+4;
+    let endSlice = bgImg.lastIndexOf('/');
+    let folderSrc = bgImg.slice(startSlice,endSlice)
+      console.log(folderSrc)
+      $('.portretContainer').empty();
+      for(el of data){
+        if(el.folder == folderSrc){
+          for(let i = 1; i<=el.count; i++){
+            $('.portretContainer').append(`<img class='fotosesionPhoto' src='./img/${folderSrc}/${i}.JPG'>`)
+          }
+        }
+      }
+  })
 })
